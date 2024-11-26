d140c57d4233407e98914da379ff812f765bfade
rakulmaria <rakulmaria@hotmail.com>
2024-11-26 13:32:26 +0100

    day 2: solve 1 task

---
+++ kattis-challenge/imperial/imperial.py
@@ -0,0 +1,11 @@
+# https://open.kattis.com/problems/measurement 
+# 
+# thou (th)
+# 1 inch (in) = 1000 thous
+# 1 foot (ft) = 12 inches
+# 1 yard (yd) = 3 feet
+# 1 chain (ch) = 22 yards
+# 1 furlong (fur) = 10 chains
+# 1 mile (mi) = 8 furlongs
+# 1 league (lea) = 3 miles
+
